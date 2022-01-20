7fbdb683cd8fcfd051e19c78c9a35d3766e775a0
amr elgendy <amr86elgendy@gmail.com>
2022-01-20 16:19:17 +0200

    fix

--- components/home/NewArrival.js
+++ components/home/NewArrival.js
@@ -7,7 +7,8 @@ const NewArrival = ({ products }) => {
     <section className='py-20'>
       <header className='py-4 bg-gray-100 rounded-t-md'>
         <h1 className='italic font-bold tracking-wider text-center text-gray-400 uppercase md:text-4xl'>
-          {t('home:latest-products')}
+          {/* {t('home:latest-products')} */}
+          latest-products
         </h1>
       </header>
       <div className='grid gap-10 my-2 sm:grid-cols-2 xmd:grid-cols-3 xl:grid-cols-4'>
